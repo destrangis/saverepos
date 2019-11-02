af8e582b4179c93a092f68910d07761e7453a123
Javier Llopis <javier@llopis.me>
2019-11-02 17:44:09 +0000

    Source formatted with black

--- saverepos.py
+++ saverepos.py
@@ -16,15 +16,16 @@ from bs4 import BeautifulSoup
 VERSION = "1.0.0"
 basedir = "repos"
 
+
 def get_repos(url):
     repos_page = url + "/explore/repos"
     rsp = requests.get(repos_page)
     rsp.raise_for_status()
 
-    #soup = BeautifulSoup(rsp.text, "html5lib")
+    # soup = BeautifulSoup(rsp.text, "html5lib")
     soup = BeautifulSoup(rsp.text, "html.parser")
     repo_links = soup.find_all("a", class_="name")
-    return [ url + lnk.attrs["href"] for lnk in repo_links ]
+    return [url + lnk.attrs["href"] for lnk in repo_links]
 
 
 def save_repo(repo_url, basedir):
@@ -38,6 +39,7 @@ def save_repo(repo_url, basedir):
         gitfile = repo_url + ".git"
         repo_clone(gitfile, basedir)
 
+
 def repo_clone(repo, basedir):
     savecdir = os.getcwd()
     os.chdir(basedir)
@@ -46,18 +48,26 @@ def repo_clone(repo, basedir):
 
     os.chdir(savecdir)
 
+
 def repo_update(repopath):
     savedir = os.getcwd()
     os.chdir(repopath)
     proc = subprocess.run(["git", "pull"])
     os.chdir(savedir)
 
+
 def cli_options(argv):
     p = argparse.ArgumentParser(description=__doc__)
-    p.add_argument("--basedir", "-d", metavar="DIR", default=basedir,
-                help="Base directory where the repositories will be held.")
+    p.add_argument(
+        "--basedir",
+        "-d",
+        metavar="DIR",
+        default=basedir,
+        help="Base directory where the repositories will be held.",
+    )
     p.add_argument("url", help="URL of the Gitea server.")
     return p.parse_args(argv)
+
 
 def main(args=None):
     if args is None:
@@ -73,5 +83,6 @@ def main(args=None):
         save_repo(rp, basedir)
     return 0
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     sys.exit(main())
--- setup.py
+++ setup.py
@@ -6,17 +6,12 @@ setup(
     name="saverepos",
     version=saverepos.VERSION,
     py_modules=["saverepos"],
-    entry_points = {
-        'console_scripts': ['saverepos=saverepos:main'],
-    },
+    entry_points={"console_scripts": ["saverepos=saverepos:main"]},
     author="Javier Llopis",
     author_email="javier@llopis.me",
     url="https://github.com/destrangis/saverepos",
     description=saverepos.__doc__,
-    install_requires = [
-        "requests>=2.18.4",
-        "beautifulsoup4>=4.8.1",
-    ],
+    install_requires=["requests>=2.18.4", "beautifulsoup4>=4.8.1"],
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3 :: Only",
@@ -31,5 +26,5 @@ setup(
         "Topic :: System :: Archiving :: Backup",
         "Topic :: System :: Archiving :: Mirroring",
         "Topic :: Utilities",
-        ]
-    )
+    ],
+)
