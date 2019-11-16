c6b17049ca4a008ed425426e75f0f54b5dfd0947
Javier Llopis <javier@llopis.me>
2019-11-16 23:34:42 +0000

    Use Gitea's REST API
    Also, use urllib.request and json, avoiding requests (it's a fairly
    simple api call) and beautifulsoup (since we now just parse json)

--- saverepos.py
+++ saverepos.py
@@ -10,36 +10,54 @@ import os
 import os.path
 import subprocess
 import sys
+import json
+import urllib.request
 
-import requests
-from bs4 import BeautifulSoup
 
-VERSION = "1.0.3"
+VERSION = "1.1.0"
 basedir = "repos"
 
 
 def get_repos(url, log):
-    repos_page = url + "/explore/repos"
-    log.debug("Retrieving: {}".format(repos_page))
-    rsp = requests.get(repos_page)
-    rsp.raise_for_status()
+    page = 1
+    while 1:
+        repos_page = url + "/api/v1/repos/search?page={}".format(page)
+        log.debug("Retrieving: {}".format(repos_page))
+        rsp = urllib.request.urlopen(repos_page)
+        content = json.load(rsp)
 
-    # soup = BeautifulSoup(rsp.text, "html5lib")
-    soup = BeautifulSoup(rsp.text, "html.parser")
-    repo_links = soup.find_all("a", class_="name")
-    return [url + lnk.attrs["href"] for lnk in repo_links]
+        if content["ok"]:
+            log.debug("ok - retrieved {} records.".format(len(content["data"])))
+        else:
+            log.error("NOT OK: {}:".format(repos_page) + json.dumps(content, indent=2))
+
+        if not content["data"]:
+            break
+
+        for repo in content["data"]:
+            yield repo
+
+        page += 1
 
 
-def save_repo(repo_url, basedir, log):
+def save_repo(repo_obj, basedir, log):
+    repo_url = repo_obj["html_url"]
+    gitfile = repo_obj["clone_url"]
+    owner = repo_obj["owner"]["login"]
+
+    user_backup = os.path.join(basedir, owner)
+    if not os.path.isdir(user_backup):
+        os.makedirs(user_backup)
+
     repodir = os.path.basename(repo_url)
-    repopath = os.path.join(basedir, repodir)
+    repopath = os.path.join(user_backup, repodir)
+
     if os.path.isdir(repopath):
         log.info("Updating {}".format(repopath))
         repo_update(repopath, log)
     else:
-        gitfile = repo_url + ".git"
         log.info("Cloning {}".format(gitfile))
-        repo_clone(gitfile, basedir, log)
+        repo_clone(gitfile, user_backup, log)
 
 
 def repo_clone(repo, basedir, log):
@@ -107,8 +125,7 @@ def main(args=None):
     if not os.path.isdir(opts.basedir):
         os.makedirs(opts.basedir)
 
-    repos = get_repos(opts.url, log)
-    for rp in repos:
+    for rp in get_repos(opts.url, log):
         save_repo(rp, opts.basedir, log)
     return 0
 
